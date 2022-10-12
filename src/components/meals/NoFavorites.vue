3db24b1d84fc3cfb70c5f7d2677d08b5d8c0f722
BramBekkers <bram.bekkers@outlook.com>
2022-10-12 18:59:37 +0200

    Added no Favorites message

---
+++ src/components/meals/NoFavorites.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="no-favorites">
+		<i class="fa-solid fa-triangle-exclamation"></i>
+		<p>There are no favorite meals.</p>
+		<p>Add your favorite meal by clicking on the heart icon!</p>
+	</div>
+</template>
+
+<style scoped lang="scss">
+.no-favorites {
+	height: 50vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+
+	i {
+		font-size: 10vw;
+		margin-bottom: 2rem;
+	}
+
+	p {
+		text-align: center;
+		max-width: 500px;
+		margin: 0;
+	}
+}
+</style>
--- src/pages/FavoriteMeals.vue
+++ src/pages/FavoriteMeals.vue
@@ -1,27 +1,36 @@
-
 <script setup lang="ts">
-import MealsList from '../components/meals/MealsList.vue'
-import { useMealsStore } from '../store'
-const mealsStore = useMealsStore()
+import MealsList from "../components/meals/MealsList.vue";
+import NoFavorites from "@/components/meals/NoFavorites.vue";
+
+import { useMealsStore } from "../store";
+const mealsStore = useMealsStore();
 </script>
 
 <template>
-    <div>
-        <h2>My Favorite Recipes</h2>
-        <MealsList :meals="mealsStore.favoriteMeals" />
-        <img class="floating-image pot" src="../assets/svg/Pot.svg" alt="cooking pot">
-    </div>
+	<div>
+		<h2>My Favorite Recipes</h2>
+		<MealsList
+			:meals="mealsStore.favoriteMeals"
+			v-if="mealsStore.favoriteMeals?.length"
+		/>
+		<NoFavorites v-else />
+		<img
+			class="floating-image pot"
+			src="../assets/svg/Pot.svg"
+			alt="cooking pot"
+		/>
+	</div>
 </template>
 
 <style lang="scss" scoped>
 h2 {
-    margin: 0;
+	margin: 0;
 }
 
 .pot {
-    right: -5%;
-    bottom: -10%;
-    width: 30vw;
-    rotate: 20deg;
+	right: -5%;
+	bottom: -10%;
+	width: 30vw;
+	rotate: 20deg;
 }
 </style>
